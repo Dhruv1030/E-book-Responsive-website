08ba1a3bea0b2bdc3d16d5679fa1b321a371e051
Dhruv Patel <dp14798@gmail.com>
2023-11-28 11:06:39 -0500

    Edits Featured Section
    Adds New Featured book section in the page

--- responsive-book-website-main/assets/js/main.js
+++ responsive-book-website-main/assets/js/main.js
@@ -74,10 +74,45 @@ breakpoints:{
 });
 
 /*=============== FEATURED SWIPER ===============*/
+let swiperFeatured = new Swiper('.featured__swiper', {
+  loop: true,
+spaceBetween: 16,
+grabCursor: true,
+slidesPerView: 'auto',
+centeredSlides: 'auto',
+
+ navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+breakpoints:{
+    1150:{
+        slidesPerView: 4,
+        centeredSlides: false,
+
+    }
+}
+
+});
+
+
 
 
 /*=============== NEW SWIPER ===============*/
+let swiperNew = new Swiper('.new__swiper', {
+  loop: true,
+spaceBetween: 16,
+slidesPerView: 'auto',
 
+breakpoints:{
+    1150:{
+        slidesPerView: 3,
+
+    }
+}
+
+});
 
 /*=============== TESTIMONIAL SWIPER ===============*/
 
